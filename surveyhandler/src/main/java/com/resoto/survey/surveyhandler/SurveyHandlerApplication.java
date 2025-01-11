080a7ff3528c0c4fab3e3e132048b2139b4ef452
marcel boelaars <m.boelaars@fontys.Nl>
2025-01-11 14:40:54 +0100

    Fixed surveyhandler/surveymanager base app with docker, including docker compose - updated gitignore to ignore data folder of mysql container

---
+++ surveyhandler/src/main/java/com/resoto/survey/surveyhandler/SurveyHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.resoto.survey.surveyhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyHandlerApplication.class, args);
+    }
+
+}
---
+++ surveyhandler/src/test/java/com/resoto/survey/surveyhandler/SurveyHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resoto.survey.surveyhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class SurveyHandlerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
